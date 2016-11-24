49085900f9d636880f344ed8b1867457fa22ed20
Jeroen Jacobs <JeroenJacobs@Outlook.com>
2016-11-24 11:48:12 +0100

    Last working bolt detection version, and working launchfile for robot_mover

--- bolt_detection/src/bolt_detection.cpp
+++ bolt_detection/src/bolt_detection.cpp
@@ -9,11 +9,11 @@
 #include <stdio.h>
 #include <math.h>
 
-#define DEFAULT_CAMERA_ID   1
-#define ESC_KEY     27
-#define CALIBRATION_FILE_PATH "/resources/out_camera_data.yml"
-#define TRANSFORM_FILE_PATH  "/resources/out_transform_data.yml"
-#define CAMERA_FRAME "my_camera_view_link" //TODO check name
+#define DEFAULT_CAMERA_ID		 1
+#define ESC_KEY					27
+#define CALIBRATION_FILE_PATH	"/resources/out_camera_data.yml"
+#define TRANSFORM_FILE_PATH 	"/resources/out_transform_data.yml"
+#define CAMERA_FRAME "camera_view_link"
 
 #define CANNY_THRESH 80
 
@@ -22,203 +22,213 @@
 #define IMAGE_HEIGHT_PX 1080
 #define IMAGE_HEIGHT_MM  450
 
-using namespace std;
-using namespace cv;
-
 //function declaration
-bool openVideo(int stream, VideoCapture * camera);
-bool getFrame(VideoCapture * camera, Mat * image);
+bool openVideo(int stream, cv::VideoCapture * camera);
+bool getFrame(cv::VideoCapture * camera, cv::Mat * image);
 bool readCalibrationData();
 bool readTransformData();
 
 //object declaration
-VideoCapture camera;
-Mat cameraMatrix, distCoeffs;
-Mat transformMatrix = Mat(3, 3, CV_32FC1);
-Mat image, imageCropped;
+cv::VideoCapture camera;
+cv::Mat cameraMatrix, distCoeffs;
+cv::Mat transformMatrix = cv::Mat(3, 3, CV_32FC1);
+cv::Mat image, imageCropped;
 geometry_msgs::PoseArray holes;
 ros::Publisher pubHoles;
 
-void addHole(int x, int y) {
-    float mm_x = (float) x * ((float) IMAGE_WIDTH_MM / IMAGE_WIDTH_PX);
-    float mm_y = (float) y * ((float) IMAGE_HEIGHT_MM / IMAGE_HEIGHT_PX);
 
-    geometry_msgs::Pose point;
+// x and y in px
+void addHole(int px_x, int px_y)
+{
+  float mm_x = (float) px_x * ((float)IMAGE_WIDTH_MM / IMAGE_WIDTH_PX);
+  float mm_y = (float) px_y * ((float)IMAGE_HEIGHT_MM / IMAGE_HEIGHT_PX);
 
-    /*point.header.stamp = ros::Time::now();
-    point.header.frame_id = CAMERA_FRAME;*/
-    point.orientation.w = 1.0;
-    point.position.x = (mm_y / 1000);
-    point.position.y = (mm_x / 1000);
+  geometry_msgs::Pose point;
 
-    ROS_DEBUG("Added hole: x[%f], y[%f].", point.position.x, point.position.y);
+  //orientation
+  point.orientation.x = 0.70711;
+  point.orientation.z = -0.70711;
 
-    holes.poses.push_back(point);
+  //position
+  point.position.x = (mm_y / 1000);
+  point.position.y = (mm_x / 1000);
+  point.position.z = 0.01;
+
+  ROS_INFO("Added hole: x[%f], y[%f].",  point.position.x,  point.position.y);
+
+  holes.poses.push_back(point);
 }
 
-Mat detectHoles(Mat * object) {
-    Mat cannyOutput, imageGray;
-    vector<vector<Point> > contours;
-    vector<Vec4i> hierarchy;
+cv::Mat detectHoles(cv::Mat * object)
+{
+  cv::Mat cannyOutput, imageGray;
+  std::vector<std::vector<cv::Point> > contours;
+  std::vector<cv::Vec4i> hierarchy;
 
-    cvtColor(*object, imageGray, CV_BGR2GRAY);
-    Canny(imageGray, cannyOutput, CANNY_THRESH, CANNY_THRESH * 3, 3);
+  cv::cvtColor(*object, imageGray, CV_BGR2GRAY);
+  cv::Canny(imageGray, cannyOutput, CANNY_THRESH, CANNY_THRESH*3, 3);
 
+  cv::findContours(cannyOutput, contours, hierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE, cv::Point(0,0));
 
-    //HoughCircles(cannyOutput, contours, CV_HOUGH_GRADIENT, 1, canny_i.rows/9, 105, 18, 0, 20 );
-    findContours(cannyOutput, contours, hierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
+  holes.poses.clear();
 
-    holes.poses.clear();
+  // Get the moments
+  std::vector<cv::Moments> mu(contours.size() );
+  for( int i = 0; i < contours.size(); i++ )
+  { mu[i] = moments( contours[i], false ); }
 
-    // Get the moments
-    vector<Moments> mu(contours.size());
-    for (int i = 0; i < contours.size(); i++) {
-        mu[i] = moments(contours[i], false);
+  ///  Get the mass centers:
+  std::vector<cv::Point2f> mc( contours.size() );
+  for( int i = 0; i < contours.size(); i++ )
+  {
+    mc[i] = cv::Point2f( mu[i].m10/mu[i].m00 , mu[i].m01/mu[i].m00 );
+    //filter circles
+
+  }
+
+  /// Draw contours
+  cv::Mat drawing = cv::Mat::zeros( cannyOutput.size(), CV_8UC3 );
+
+  for( int i = 0; i< contours.size(); i++ )
+  {
+    if (mu[i].m00 > 10)
+    {
+      //printf(" * Contour[%d] - Area (M_00) = %.2f - Area OpenCV: %.2f - Length: %.2f \n", i, mu[i].m00, contourArea(contours[i]), arcLength( contours[i], true ) );
+      cv::Scalar color = cv::Scalar(0, 255, 255);
+      cv::drawContours( drawing, contours, i, color, 2, 8, hierarchy, 0, cv::Point() );
+      cv::circle( drawing, mc[i], 4, color, -1, 8, 0 );
+
+      addHole(mc[i].x, mc[i].y);
     }
-
-    ///  Get the mass centers:
-    vector<Point2f> mc(contours.size());
-    for (int i = 0; i < contours.size(); i++) {
-        mc[i] = Point2f(mu[i].m10 / mu[i].m00, mu[i].m01 / mu[i].m00);
-        //filter circles
-    }
-
-    /// Draw contours
-    Mat drawing = Mat::zeros(cannyOutput.size(), CV_8UC3);
-
-    for (int i = 0; i < contours.size(); i++) {
-        if (mu[i].m00 > 10) {
-            printf(" * Contour[%d] - Area (M_00) = %.2f - Area OpenCV: %.2f - Length: %.2f \n", i, mu[i].m00, contourArea(contours[i]), arcLength(contours[i], true));
-            Scalar color = Scalar(0, 255, 255);
-            drawContours(drawing, contours, i, color, 2, 8, hierarchy, 0, Point());
-            circle(drawing, mc[i], 4, color, -1, 8, 0);
-
-            addHole(mc[i].x, mc[i].y);
-        }
-
-        //        Point center(cvRound(contours[i][0]), cvRound(contours[i][1]));
-        //        int radius = cvRound(contours[i][2]);
-        //        // circle center
-        //        circle(drawing, center, 3, Scalar(0, 255, 0), -1, 8, 0);
-        //        // circle outline
-        //        circle(drawing, center, radius, Scalar(0, 0, 255), 3, 8, 0);
-
-
-    }
-    return drawing;
+  }
+  return drawing;
 }
 
-void sendHoles(void) {
-    //send holes
-    holes.header.stamp = ros::Time::now();
-    pubHoles.publish(holes);
+void sendHoles(void)
+{
+  /*if (holes.poses.size() == 0)
+    return;*/
 
-    ROS_DEBUG("Sent %i holes.", (int) holes.poses.size());
+  //send holes
+  holes.header.frame_id = CAMERA_FRAME;
+  holes.header.stamp = ros::Time::now();
+  pubHoles.publish(holes);
+
+  ROS_DEBUG("Sent %i holes.", (int)holes.poses.size());
 }
 
-int main(int argc, char *argv[]) {
-    ros::init(argc, argv, "bolt_detection_node");
-    ros::NodeHandle n;
+int main(int argc, char *argv[])
+{
+  ros::init(argc, argv, "bolt_detection_node");
+  ros::NodeHandle n;
 
-    pubHoles = n.advertise<geometry_msgs::PoseArray>("/holes", 1);
-    ros::Timer timer = n.createTimer(ros::Duration(2.0), boost::bind(&sendHoles));
+  pubHoles = n.advertise<geometry_msgs::PoseArray>("/holes", 1);
+  ros::Timer timer = n.createTimer(ros::Duration(2.0), boost::bind(&sendHoles));
 
-    ROS_INFO("Bolt Detection Node Started!");
+  ROS_INFO("Bolt Detection Node Started!");
 
-    ros::Rate r(30);
+  ros::Rate r(30);
 
-    //load camera
-    int cameraId = (argc > 1) ? atoi(argv[1]) : DEFAULT_CAMERA_ID;
-    if (!openVideo(cameraId, &camera))
-        exit(1);
+  //load camera
+  int cameraId = (argc > 1) ? std::atoi(argv[1]) : DEFAULT_CAMERA_ID;
+  if (!openVideo(cameraId, &camera))
+    exit(1);
 
-    //create window
-    //namedWindow("Camera", CV_WINDOW_NORMAL);
-    //resizeWindow("Camera", 960, 540);
-    namedWindow("CameraCropped", CV_WINDOW_NORMAL);
-    resizeWindow("CameraCropped", 515, 540);
+  //create window
+  //cv::namedWindow("Camera", CV_WINDOW_NORMAL);
+  //cv::resizeWindow("Camera", 960, 540);
+  cv::namedWindow("CameraCropped", CV_WINDOW_NORMAL);
+  cv::resizeWindow("CameraCropped", 515, 540);
 
-    if (!readCalibrationData()) return 1;
-    if (!readTransformData()) return 1;
+  if(!readCalibrationData()) return 1;
+  if(!readTransformData()) return 1;
 
-    while (ros::ok() && getFrame(&camera, &image)) {
-        //undistort image
-        Mat imageU = image.clone();
-        undistort(image, imageU, cameraMatrix, distCoeffs);
+  while(ros::ok() && getFrame(&camera, &image))
+  {
+    //undistort image
+    cv::Mat imageU = image.clone();
+    cv::undistort(image, imageU, cameraMatrix, distCoeffs);
 
-        //transform image
-        Mat perspectiveImage = Mat::zeros(imageU.rows, imageU.cols, imageU.type());
-        warpPerspective(imageU, perspectiveImage, transformMatrix, perspectiveImage.size());
+    //transform image
+    cv::Mat perspectiveImage = cv::Mat::zeros(imageU.rows, imageU.cols, imageU.type());
+    warpPerspective(imageU, perspectiveImage, transformMatrix, perspectiveImage.size());
 
-        imageCropped = perspectiveImage(Rect(0, 0, IMAGE_WIDTH_PX, IMAGE_HEIGHT_PX));
+    imageCropped = perspectiveImage(cv::Rect(0, 0, IMAGE_WIDTH_PX, IMAGE_HEIGHT_PX));
 
-        //detect holes
-        Mat detected = detectHoles(&imageCropped);
+    //detect holes
+    cv::Mat detected = detectHoles(&imageCropped);
 
-        //imshow("Camera", imageU);
-        imshow("CameraCropped", detected);
+    //cv::imshow("Camera", imageU);
+    cv::imshow("CameraCropped", detected);
 
-        if ((cvWaitKey(40)&0xff) == ESC_KEY) {
-            cout << "---------------------------------------------" << endl;
-            destroyWindow("Camera");
-            destroyWindow("CameraCropped");
+    if((cvWaitKey(40)&0xff)==ESC_KEY)
+    {
+      std::cout << "---------------------------------------------" << std::endl;
+      cv::destroyWindow("Camera");
+      cv::destroyWindow("CameraCropped");
 
-            ros::shutdown();
-        }
-
-        ros::spinOnce();
-        r.sleep();
+      ros::shutdown();
     }
+
+    ros::spinOnce();
+    r.sleep();
+  }
 }
 
-bool readCalibrationData() {
-    //load camera parameters from file
-    string pathname = ros::package::getPath("bolt_detection") + CALIBRATION_FILE_PATH;
-    FileStorage fs(pathname, FileStorage::READ);
-    if (!fs.isOpened()) {
-        cerr << "Failed to open " << CALIBRATION_FILE_PATH << endl;
+bool readCalibrationData()
+{
+  //load camera parameters from file
+  std::string pathname = ros::package::getPath("bolt_detection") + CALIBRATION_FILE_PATH;
+  cv::FileStorage fs(pathname, cv::FileStorage::READ);
+  if (!fs.isOpened())
+  {
+    std::cerr << "Failed to open " << CALIBRATION_FILE_PATH << std::endl;
+    return false;
+  }
+  fs["camera_matrix"] >> cameraMatrix;
+  fs["distortion_coefficients"] >> distCoeffs;
+
+  std::cout << "Loaded calibration data from [" << CALIBRATION_FILE_PATH << "]." << std::endl;
+  return true;
+}
+
+bool readTransformData()
+{
+  std::string pathname = ros::package::getPath("bolt_detection") + TRANSFORM_FILE_PATH;
+  cv::FileStorage fs(pathname, cv::FileStorage::READ);
+  if (!fs.isOpened())
+    {
+        std::cerr << "Failed to open " << TRANSFORM_FILE_PATH << std::endl;
         return false;
     }
-    fs["camera_matrix"] >> cameraMatrix;
-    fs["distortion_coefficients"] >> distCoeffs;
+  fs["transform_matrix"] >> transformMatrix;
 
-    cout << "Loaded calibration data from [" << CALIBRATION_FILE_PATH << "]." << endl;
-    return true;
-}
-
-bool readTransformData() {
-    string pathname = ros::package::getPath("bolt_detection") + TRANSFORM_FILE_PATH;
-    FileStorage fs(pathname, FileStorage::READ);
-    if (!fs.isOpened()) {
-        cerr << "Failed to open " << TRANSFORM_FILE_PATH << endl;
-        return false;
-    }
-    fs["transform_matrix"] >> transformMatrix;
-
-    cout << "Loaded transform data from [" << TRANSFORM_FILE_PATH << "]." << endl;
-    return true;
+  std::cout << "Loaded transform data from [" << TRANSFORM_FILE_PATH << "]." << std::endl;
+  return true;
 }
 
 //try to open an video with the given filename
+bool openVideo(int stream, cv::VideoCapture * camera)
+{
+  camera->open(stream);
+  if(!camera->isOpened())
+  {
+    std::cout << "Could not open camera ID [" << stream << "]." << std::endl;
+    return false;
+  }
 
-bool openVideo(int stream, VideoCapture * camera) {
-    camera->open(stream);
-    if (!camera->isOpened()) {
-        cout << "Could not open camera ID [" << stream << "]." << endl;
-        return false;
-    }
+  //optional: set resolution
+  camera->set(CV_CAP_PROP_FRAME_WIDTH, 1920);
+  camera->set(CV_CAP_PROP_FRAME_HEIGHT, 1080);
 
-    //optional: set resolution
-    camera->set(CV_CAP_PROP_FRAME_WIDTH, 1920);
-    camera->set(CV_CAP_PROP_FRAME_HEIGHT, 1080);
-
-    Size refS = Size((int) camera->get(CV_CAP_PROP_FRAME_WIDTH),
-            (int) camera->get(CV_CAP_PROP_FRAME_HEIGHT));
-    cout << "Loaded camera ID [" << stream << "], size [" << refS.width << "x" << refS.height << "]" << endl;
-    return true;
+  cv::Size refS = cv::Size((int) camera->get(CV_CAP_PROP_FRAME_WIDTH),
+  (int) camera->get(CV_CAP_PROP_FRAME_HEIGHT));
+  std::cout << "Loaded camera ID [" << stream << "], size [" << refS.width << "x" << refS.height << "]" << std::endl;
+  return true;
 }
 
-bool getFrame(VideoCapture * camera, Mat * image) {
-    camera->read(*image);
-    return !image->empty();
+bool getFrame(cv::VideoCapture * camera, cv::Mat * image)
+{
+  camera->read(*image);
+  return !image->empty();
 }
