e4e7c28479806894fbfea068da4c681f338ef55a
Tycho Hertogs <tycho94@hotmail.com>
2016-11-25 13:35:51 +0100

    tf & urdf fix

--- robot_mover/src/Path_Publisher.cpp
+++ robot_mover/src/Path_Publisher.cpp
@@ -11,50 +11,47 @@
 ros::Publisher publisher;
 geometry_msgs::Pose get_Pose(double pX, double pY, double pZ, double oX, double oY, double oZ, double oW);
 
-int main(int argc, char *argv[])
-{
-	/* code */
-  ros::init(argc, argv, NODE_NAME);
-  ros::NodeHandle n;
+int main(int argc, char *argv[]) {
+    /* code */
+    ros::init(argc, argv, NODE_NAME);
+    ros::NodeHandle n;
 
-  geometry_msgs::Pose pose1, pose2,pose3 ,pose4;
-  geometry_msgs::PoseArray pathArray;
+    geometry_msgs::Pose pose1, pose2, pose3, pose4;
+    geometry_msgs::PoseArray pathArray;
 
-  //Poses gotten from transform: camera_view_link --> tool0
-  pose1 = get_Pose(0.2, 0.2, 0.05,0.707, -0.000, -0.707, 0.000);//1
-  /*pose2 = get_Pose(0.317, 0.183, 0.115, 0.707, -0.000, -0.707, 0.000);//Move back To Home
-  pose3 = get_Pose(0.405, 0.170, 0.162,  0.707, -0.000, -0.707, 0.000);//4
-  pose4 = get_Pose(0.197, 0.165, 0.025,  0.707, -0.000, -0.707, 0.000); //5*/
-        
-  pathArray.poses.push_back(pose1);
-  /*pathArray.poses.push_back(pose2);
-  pathArray.poses.push_back(pose3);
-  pathArray.poses.push_back(pose4);*/
-  pathArray.header.frame_id= CAMERA_VIEW_FRAME;
+    //Poses gotten from transform: camera_view_link --> tool0
+    pose1 = get_Pose(0.2, 0.2, 0.05, 0.707, -0.000, -0.707, 0.000); //1
+    /*pose2 = get_Pose(0.317, 0.183, 0.115, 0.707, -0.000, -0.707, 0.000);//Move back To Home
+    pose3 = get_Pose(0.405, 0.170, 0.162,  0.707, -0.000, -0.707, 0.000);//4
+    pose4 = get_Pose(0.197, 0.165, 0.025,  0.707, -0.000, -0.707, 0.000); //5*/
 
-  publisher = n.advertise<geometry_msgs::PoseArray>(SUBSCRIBER_TOPIC, 1);
-  ros::Rate rate(1);
+    pathArray.poses.push_back(pose1);
+    /*pathArray.poses.push_back(pose2);
+    pathArray.poses.push_back(pose3);
+    pathArray.poses.push_back(pose4);*/
+    pathArray.header.frame_id = CAMERA_VIEW_FRAME;
 
-  while(ros::ok())
-  {
-    publisher.publish(pathArray);
-    ros::spinOnce();
-    rate.sleep();
-  }
+    publisher = n.advertise<geometry_msgs::PoseArray>(SUBSCRIBER_TOPIC, 1);
+    ros::Rate rate(1);
 
-	return 0;
+    while (ros::ok()) {
+        publisher.publish(pathArray);
+        ros::spinOnce();
+        rate.sleep();
+    }
+
+    return 0;
 }
 
-geometry_msgs::Pose get_Pose(double pX, double pY, double pZ, double oX, double oY, double oZ, double oW)
-{
-  geometry_msgs::Pose pose;
-  pose.position.x = pX;
-  pose.position.y = pY;
-  pose.position.z = pZ;
+geometry_msgs::Pose get_Pose(double pX, double pY, double pZ, double oX, double oY, double oZ, double oW) {
+    geometry_msgs::Pose pose;
+    pose.position.x = pX;
+    pose.position.y = pY;
+    pose.position.z = pZ;
 
-  pose.orientation.x = oX;
-  pose.orientation.y = oY;
-  pose.orientation.z = oZ;
-  pose.orientation.w = oW;
-  return pose;
+    pose.orientation.x = oX;
+    pose.orientation.y = oY;
+    pose.orientation.z = oZ;
+    pose.orientation.w = oW;
+    return pose;
 }
