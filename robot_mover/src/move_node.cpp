8a9105c768b8dc7007983205ad835ba9a8772eb8
Rodriguez Sandio Fongang <juniorsandio@gmail.com>
2016-11-25 11:49:03 -0600

    move_node modified

--- robot_mover/src/move_node.cpp
+++ robot_mover/src/move_node.cpp
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
   ros::NodeHandle n;  
   moveit::planning_interface::MoveGroup group(GROUPID);
   move_handler *obj= new move_handler(&group);
-  ros::Subscriber sub = n.subscribe(SUBSCRIBER_TOPIC,SUBSCRIBER_BUFFER_SIZE, &MoveHandler::onHolesMessageReceived,obj);
+  ros::Subscriber sub = n.subscribe(SUBSCRIBER_TOPIC,SUBSCRIBER_BUFFER_SIZE, &move_handler::onHolesMessageReceived,obj);
   
   while(ros::ok()){
 
