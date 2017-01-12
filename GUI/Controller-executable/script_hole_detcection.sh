ee6ee844f36ca5c486477740b084027e8c859ff0
Tycho Hertogs <tycho94@hotmail.com>
2017-01-12 14:25:29 +0300

    scriptfix

--- GUI/Controller-executable/script_hole_detcection.sh
+++ GUI/Controller-executable/script_hole_detcection.sh
@@ -1,11 +1,7 @@
-#!/bin/bash
+#!/bin/sh
  
 #dir=`find / -name $2 2>/dev/null`
 echo $2
 cd $2
 source devel/setup.bash
-var='rostopic pub  /detect_cmd bolt_detection/Detection  "detect:'$1
-var=$var'" -1'
-echo $var
-xterm -hold -e $var &
-
+rostopic pub /detect_cmd bolt_detection/Detection  "detect: true" -1
